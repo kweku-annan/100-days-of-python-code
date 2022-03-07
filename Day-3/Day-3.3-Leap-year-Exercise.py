db08122f4903c15f7f76984c14216753e9971c97
kweku-annan <kwekuannan.dev@gmail.com>
2022-03-07 21:08:39 +0000

    Add the and finish the leap-year coding challenge

--- Day-3/Day-3.3-Leap-year-Exercise.py
+++ Day-3/Day-3.3-Leap-year-Exercise.py
@@ -7,9 +7,13 @@ This is how you work out if a particular year is a leap year:
 On every year that is evenly divisible by 4 EXCEPT every year that is evenly divisible by 100 UNLESS
 the year is also divisible by 400
 """
-year = int(input("Which year do you want to check?\n"))
 
-if year % 4 == 0 and year % 100 != 0 and year % 400 == 0:
-    print(f"The year {year} is a leap year.")
-else:
-    print(f"The year {year} is not a leap year")
+while True:
+    year = int(input("Which year do you want to check?\n"))
+
+    if year % 4 == 0:
+        if year % 100 != 0 or year % 400 == 0:
+            # if year % 400 == 0:
+            print(f"The year {year} is a leap year.")
+    else:
+        print(f"The year {year} is not a leap year.")
