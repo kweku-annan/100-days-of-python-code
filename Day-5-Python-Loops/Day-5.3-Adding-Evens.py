efc73133260f193c4a0f26ec4ec7bc21c0268633
kweku-annan <kwekuannan.dev@gmail.com>
2022-03-16 09:59:37 +0000

    Added solution for Day-5.3 coding challenge

---
+++ Day-5-Python-Loops/Day-5.3-Adding-Evens.py
@@ -0,0 +1,15 @@
+"""
+Adding Evens
+
+INSTRUCTIONS
+You are going to write a program that calculates the sum of all the even numbers from 1 to 100, including 1 and 100.
+
+Important, there should only be 1 print statement in your console ouput.
+It should just print the final total and not every step of the calculation.
+
+"""
+one_hundred = range(101)
+total = 0
+for number in one_hundred:
+    total += number
+print(total)
