77cd7e26d2fc59f8c61d67b70a22b8f0edb0b41b
kweku-annan <kwekuannan.dev@gmail.com>
2022-04-12 23:12:44 +0000

    Merged the encrypt and decrypt function as caesar function. About to delete the encrypt and decrypt functions

--- Day-8-Beginner-Function-Parameters-and-Caesar-Cipher/caesar-cipher.py
+++ Day-8-Beginner-Function-Parameters-and-Caesar-Cipher/caesar-cipher.py
@@ -1,6 +1,27 @@
 # TODO: Modify to include numbers
 
+def caeser(start_message, shift_message, direction):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+    end_message = ""
+    for letter in start_message:
+        if letter == " " or letter not in alphabet:
+            end_message += letter
+        else:
+            letter_index = alphabet.index(letter)
+            if direction == "decode":
+                shift_message *= -1
+            letter_index_shifted = letter_index + shift_message
+            alphabet_length = len(26)
+            if letter_index_shifted >= alphabet_length:
+                letter_index_shifted %= alphabet_length
+            new_letter = alphabet[letter_index_shifted]
+            end_message += new_letter
+    print(f"Here's the {direction}d result: {end_message}")
+
+
 def encrypt(raw_message, shift_raw):
+    """This function is responsible for encrypting a message from user input"""
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                 'v', 'w', 'x', 'y', 'z']
     encoded_word = []
@@ -11,7 +32,7 @@ def encrypt(raw_message, shift_raw):
             letter_index = alphabet.index(letter)  # Get the index of current letter under loop
             letter_index_shifted = letter_index + shift_raw  # Shift the letter by adding shift to the letter's index
             alphabet_length = len(alphabet)  # Find the length of the alphabet list
-            if letter_index_shifted >= alphabet_length:
+            while letter_index_shifted >= alphabet_length:
                 letter_index_shifted -= alphabet_length
             encoded_letter = alphabet[letter_index_shifted]
             encoded_word.append(encoded_letter)
@@ -19,6 +40,7 @@ def encrypt(raw_message, shift_raw):
 
 
 def decrypt(encrypted_message, shift_encrypted):
+    "This function is responsible for decrypting a message from a user input"
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                 'v', 'w', 'x', 'y', 'z']
     decoded_word = []
@@ -28,11 +50,11 @@ def decrypt(encrypted_message, shift_encrypted):
         else:
             letter_index = alphabet.index(letter)
             letter_index_shifted = letter_index - shift_encrypted
-            print(letter_index_shifted)
+            # print(letter_index_shifted)
             alphabet_length = len(alphabet)
-            if letter_index_shifted >= alphabet_length:
+            while letter_index_shifted >= alphabet_length:
                 letter_index_shifted += alphabet_length
-                print(letter_index_shifted)
+                # print(letter_index_shifted)
             decoded_letter = alphabet[letter_index_shifted]
             decoded_word.append(decoded_letter)
     return "".join(decoded_word)
@@ -44,14 +66,20 @@ while game_continue:
     if encode_decode == "encode":
         message = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
-        print(f"Here's the encoded result: ")
+        encoded = encrypt(message, shift)
+        print(f"Here's the encoded result: {encoded}")
     elif encode_decode == "decode":
         message = input("Type your message:\n").lower()
         shift = int(input("Type the shift number"))
-        print(f"Here's the decoded result: ")
+        decoded = decrypt(message, shift)
+        print(f"Here's the decoded result: {decoded}")
     else:
         print("Sorry, I didn't get that. Kindly check your input.")
 
     go_again = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
     if go_again == "no":
         game_continue = False
+
+# message = input("message").lower()
+# shift = int(input("shift: "))
+# print(encrypt(message, shift))
